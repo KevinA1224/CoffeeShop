73419950317e54ad5909cf26419f9833dea3c589
KevinA1224 <KevinA1224@users.noreply.github.com>
2022-11-25 22:48:14 +0100

    menu list update
    An update for the list of menu items. Selecting a menu item is now a separate button. Another button was added to later add the menu item to the order. A detailed view of the menu item will now be displayed to the right of the menu list. A history of the selected items, which can be deleated, appears to the right of the detail view. The menu is now displayed in its own view (for the customer) so that we can later add a separate view for the baristas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuItemsComponent } from './menu-items/menu-items.component';
 import { MenuComponent } from './menu/menu.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuItemsComponent,
-    MenuComponent
+    MenuComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from '../MenuItem';
-import { MENUITEMS } from '../mock-menu-items';
+import { MenuItemService } from '../menu-item.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,16 +10,24 @@ import { MENUITEMS } from '../mock-menu-items';
 })
 export class MenuComponent implements OnInit {
 
-  items = MENUITEMS;
+  items: MenuItem[] = [];
 
   selectedItem?: MenuItem;
-  onSelect(item: MenuItem): void {
-    this.selectedItem = item;
-  }
 
-  constructor() { }
+  constructor(private menuItemService: MenuItemService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(item: MenuItem): void {
+    this.selectedItem = item;
+    this.messageService.add(`name=${item.name}`);
+  }
+
+  getHeroes(): void {
+    this.menuItemService.getMenuItems()
+      .subscribe(items => this.items = items);
   }
 
 }
